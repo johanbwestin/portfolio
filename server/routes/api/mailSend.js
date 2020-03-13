61cdde92860e4e830aaaa04f0206f5ced4ea931b
Johan Westin <johan.b.westin@gmail.com>
2020-03-13 21:02:15 +0000

    Fixed sendGrid mail

--- server/routes/api/mailSend.js
+++ server/routes/api/mailSend.js
@@ -1,26 +1,26 @@
 const express = require('express')
 const sgMail = require('@sendgrid/mail')
+const mailTo = process.env.MAIL_TO
+const sgApi = process.env.SENDGRID_API
 
-sgMail.setApiKey(process.env.SENDGRID_API)
+sgMail.setApiKey(sgApi)
 
 const router = express.Router()
 
 // Send Mail
 router.post('/', (req, res) => {
   const msg = {
-    to: process.env.MAIL,
+    to: mailTo,
     from: req.body.email,
     subject: 'Namn: ' + req.body.name + '. ' + req.body.subject,
     text: req.body.text,
     html: '<strong>' + req.body.text + '</strong>',
   }
-  
+
   sgMail.send(msg).then(() => {
-    console.log('EMAIL SENT')
     return res.json({ result: "success", message: 'Email has been sent' })
   })
   .catch((err) => {
-    console.log('ERROR')
     return res.json({
       result: "error",
       message: err.message
