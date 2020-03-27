3df51fe69df3e2e05707bd723e934b093b922b88
Johan <johan.b.westin@gmail.com>
2020-03-27 20:09:14 +0100

    safari test

--- webpack.prod.js
+++ webpack.prod.js
@@ -37,18 +37,6 @@ module.exports = merge(common, {
       new OptimizeCssAssetsPlugin(),
       new TerserPlugin({
         terserOptions: {
-          ecma: undefined,
-          warnings: false,
-          parse: {},
-          compress: {},
-          mangle: true, // Note `mangle.properties` is `false` by default.
-          module: false,
-          output: null,
-          toplevel: false,
-          nameCache: null,
-          ie8: false,
-          keep_classnames: undefined,
-          keep_fnames: false,
           safari10: true,
         },
       })
