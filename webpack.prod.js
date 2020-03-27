8c4a1cc3093164b5ccb72e4d599b5f4e030beb1b
Johan <johan.b.westin@gmail.com>
2020-03-27 20:01:48 +0100

    ie/safari test

--- webpack.prod.js
+++ webpack.prod.js
@@ -35,7 +35,23 @@ module.exports = merge(common, {
   optimization: {
     minimizer: [
       new OptimizeCssAssetsPlugin(),
-      new TerserPlugin()
+      new TerserPlugin({
+        terserOptions: {
+          ecma: undefined,
+          warnings: false,
+          parse: {},
+          compress: {},
+          mangle: true, // Note `mangle.properties` is `false` by default.
+          module: false,
+          output: null,
+          toplevel: false,
+          nameCache: null,
+          ie8: false,
+          keep_classnames: undefined,
+          keep_fnames: false,
+          safari10: true,
+        },
+      })
     ],
     // splitChunks: {
     //   // Must be specified for HtmlWebpackPlugin to work correctly.
